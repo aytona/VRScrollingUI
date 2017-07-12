adeae0e9708886fe078f47816b2e6f96d021352e
Christopher Aytona <christopher.aytona@gmail.com>
2017-07-11 22:15:42 -0400

    Rotating object

--- Assets/Scripts/VelocitySpin.cs
+++ Assets/Scripts/VelocitySpin.cs
@@ -1,8 +1,33 @@
 ﻿using UnityEngine;
 
-
-
+[RequireComponent(typeof(Rigidbody))]
 public class VelocitySpin : MonoBehaviour {
 
-	
+    private Vector3 m_InitCast;
+    private Vector3 m_FinalCast;
+
+    private Rigidbody m_rb;
+
+    private void Start() {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            m_InitCast = Input.mousePosition;
+        } else if (Input.GetButtonUp("Fire1")) {
+            m_FinalCast = Input.mousePosition;
+            Rotate(CalculateForce(m_InitCast, m_FinalCast));
+        }
+    }
+
+    // Calculates the force applied to the rotation using the initial vector and the final vector
+    private Vector3 CalculateForce(Vector3 init, Vector3 final) {
+        return 10*(init - final);
+    }
+
+    // Rotates the actual UI using force
+    private void Rotate(Vector3 force) {
+        m_rb.AddTorque(0, -force.y, 0);
+    }
 }
